cad9250d3f9c090ae850a40501853d55b133cb9d
Felipe Francisco <fefarias97@gmail.com>
2021-12-04 10:22:59 -0300

    Exercício ascii

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int i=32;
+char letra;
+
+Console.WriteLine("Hello, World!");
+
+
+while(i<=126){
+
+    letra = Convert.ToChar(i);
+    Console.Write($"{letra}");
+
+    i++;
+
+}
+
+
